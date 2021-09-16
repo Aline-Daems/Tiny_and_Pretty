c005a47dd877c0e5f0b312cdcad47f09d0ece86e
Corduant Patricia <patricia.corduant@gmail.com>
2021-09-16 15:29:54 +0200

    navbar

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -150,4 +150,21 @@ document.querySelectorAll('[class*="reveal-"]').forEach(function(r) {
     observer.observe(r)
 })
 
+/////////////////////////////////////////////////////////////////
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+
+        let headerH = $('.header-container').outerHeight(true);
+        console.log(headerH);
+//this will calculate header's full height, with borders, margins, paddings
+        let scrollVal = $(this).scrollTop();
+        if ( scrollVal > headerH ) {
+            $('#home-banner').css({'position':'fixed','top' :'0px'});
+        } else {
+            $('#home-banner').css({'position':'static','top':'0px'});
+        }
+    });
+});
+
 
