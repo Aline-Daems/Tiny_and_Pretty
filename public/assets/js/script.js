c83c1c11174f937208ca645fded4825f252a5497
Corduant Patricia <patricia.corduant@gmail.com>
2021-10-04 09:18:33 +0200

    reglages header footer

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -147,6 +147,79 @@ document.querySelectorAll('a.js-wish').forEach(function (link) {
     });
 });
 
+//////////////////////////////////////////CAROUSEL
+
+/*
+    Carousel
+*/
+/*
+$('#multi-item-example').carousel({
+    interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    let minPerSlide = 3;
+    let next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (let i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
+
+ */
+/*
+$('#multi-item-example').on('slide.bs.carousel', function (e) {
+
+    let $e = $(e.relatedTarget);
+    let idx = $e.index();
+    let itemsPerSlide = 4;
+    let totalItems = $('.carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        let it = itemsPerSlide - (totalItems - idx);
+        for (let i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+ */
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+    interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+    let next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length>0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+});
+
 
 
 
